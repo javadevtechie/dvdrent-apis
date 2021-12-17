7dd89cd58c1e53bcb1034fd5e71508b175f1fba4
javadevtechie <95735076+shamalajames@users.noreply.github.com>
2021-12-17 16:50:49 +0530

    added controller to project

--- src/main/java/com/dvd/controller/ActorController.java
+++ src/main/java/com/dvd/controller/ActorController.java
@@ -1,5 +1,10 @@
 package com.dvd.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("movies")
 public class ActorController {
 
 }
